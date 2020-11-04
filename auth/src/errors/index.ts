32f769b0f0ab74b4cb22bf4f0dcace64f0e0583d
Arthur Araujo <aaraujjo@outlook.com.br>
2020-11-04 12:47:57 -0300

    create errors validation in auth service

---
+++ auth/src/errors/index.ts
@@ -0,0 +1,8 @@
+import { CustomError } from './custom-error'
+import { RequestValidationError } from './request-validation-error'
+import { DatabaseConnectionError } from './database-connection-error'
+export {
+  CustomError,
+  RequestValidationError,
+  DatabaseConnectionError
+}
