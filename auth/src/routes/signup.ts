9cf19500012511fa396a423711aaeb4911b3dc7f
Arthur Araujo <aaraujjo@outlook.com.br>
2020-11-04 12:56:15 -0300

    :wrench:

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,26 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send('Hi there')
+router.post('/api/users/signup', [
+  body('email')
+    .isEmail()
+    .withMessage('Email must be valid'),
+  body('password')
+    .trim()
+    .isLength({min: 4, max: 20})
+    .withMessage('Password must be between 4 and 20 characters')
+],
+(req: Request, res: Response) => {
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    throw new RequestValidationError(errors.array());
+  }
+
+  const { email, password } = req.body;
 });
 
 export { router as signupRouter}
